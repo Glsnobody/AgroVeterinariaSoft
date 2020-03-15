362efd52cf1b65afdfbae9f247cb6426ec8df885
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2020-03-15 09:52:23 -0400

    Agregando la entidad Ventas y su detalle

---
+++ Models/Ventas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgroVeterinariaSoft.Models
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        [Required(ErrorMessage = "Es necesario especificar un Cliente")]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "Es necesario establecer una fecha")]
+        [Range(typeof(DateTime),minimum:"1/1/2018",maximum:"1/1/2025",ErrorMessage ="La fecha esta fuera de rango")]
+        public DateTime Fecha { get; set; }
+        [StringLength(maximumLength:100,ErrorMessage ="La observacion es muy larga")]
+        public string Observacion { get; set; }
+        [Required(ErrorMessage = "Es necesario establecer un total")]
+        [Range(typeof(decimal),minimum:"0",maximum:"1000000000000000",ErrorMessage ="Total esta fuera de rango")]
+        public decimal Total { get; set; }
+        [Required(ErrorMessage = "Es necesario especificar la cantidad de Itbis")]
+        [Range(typeof(decimal), minimum: "0", maximum: "1000000000000000", ErrorMessage = "Itbis esta fuera de rango")]
+        public decimal Itbis { get; set; }
+        [Required(ErrorMessage = "Es necesario definir un tipo de compra")]
+        [Range(typeof(decimal), minimum: "1", maximum: "2", ErrorMessage = " El tipo de compra es incorrecto")]
+        public int TipoCompra { get; set; }
+        [ForeignKey("VentaId")]
+        public List<VentasDetalle> Productos { get; set; }
+
+        public Ventas()
+        {
+            VentaId = 0;
+            ClienteId = 0;
+            Fecha = DateTime.Now;
+            Observacion = string.Empty;
+            Total = 0;
+            Itbis = 0;
+            TipoCompra = 1;
+            Productos = new List<VentasDetalle>();
+        }
+    }
+}
---
+++ Models/VentasDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgroVeterinariaSoft.Models
+{
+    public class VentasDetalle
+    {
+        [Key]
+        public int VentaDetalleID { get; set; }
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public VentasDetalle()
+        {
+            VentaDetalleID = 0;
+            VentaId = 0;
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Precio = 0;
+            Cantidad = 0;
+        }
+        
+    }
+}
