34301d6972ea1ba5de5996b8a7f64c1887433d54
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-03-15 16:38:39 -0400

    Agregando los controolers de Compras

---
+++ Controllers/ComprasController.cs
@@ -0,0 +1,17 @@
+﻿using AgroVeterinariaSoft.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgroVeterinariaSoft.Controllers
+{
+    public class ComprasController
+    {
+        public static void Insertar(Compras Compra)
+        {
+
+        }
+
+    }
+}
