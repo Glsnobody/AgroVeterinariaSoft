382be4ada2d1ed0038af739c38813e6f339952b3
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2020-03-15 10:14:49 -0400

    Agregando Blazored toast

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,8 @@ namespace AgroVeterinariaSoft
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
