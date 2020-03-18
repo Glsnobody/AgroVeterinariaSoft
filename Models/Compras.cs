b9cbd83411d921f03d95614771c0bc7dff1c9439
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-03-17 20:39:54 -0400

    Agregando Validaciones

--- Models/Compras.cs
+++ Models/Compras.cs
@@ -12,10 +12,22 @@ namespace AgroVeterinariaSoft.Models
     {
         [Key]
         public int CompraId { get; set; }
+        [Required(ErrorMessage ="Es necesario introducir un suplidor")]
+        [Range(typeof(int), minimum: "0", maximum: "100", ErrorMessage = "El suplidorId esta fuera de rango")]
         public int SuplidorId { get; set; }
+
+        [Required(ErrorMessage = "Es necesario introducir la fecha actual")]
+        [Range(typeof(DateTime), minimum: "1/1/1990", maximum: "1/1/2030", ErrorMessage = "La fecha de compra esta fuera de rango")]
         public DateTime Fecha { get; set; }
+
+        //[Required(ErrorMessage = "Es necesario Agregar ")]
+        [Range(typeof(decimal), minimum: "0", maximum: "100", ErrorMessage = "El impuesto esta fuera de rango")]
         public decimal Impuesto { get; set; }
+
+        [Required(ErrorMessage = "Esnecesario tener un Total")]
+        [Range(typeof(decimal), minimum: "0", maximum: "100", ErrorMessage = "El total esta fuera de rango")]
         public decimal Total { get; set; }
+        
 
         [ForeignKey("CompraId")]
         public virtual List<DetalleProductos> ListaProductos { get; set; }
